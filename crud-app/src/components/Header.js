5de4eb86d4924fd0ed561517dc03ffb63c554d2b
hiimlo <hazarikalola@gmail.com>
2022-08-16 20:23:23 -0400

    added font for title

--- crud-app/src/components/Header.js
+++ crud-app/src/components/Header.js
@@ -1,15 +1,16 @@
 import './Header.css';
 import { NavLink } from 'react-router-dom';
+import './Letter Magic.ttf';
 
 export default function Header() {
     return(
         <header>
-            <h1>Crud App</h1>
+            <div className='title'>Note Board</div>
             <div className='links'>
                 <NavLink to="/" className='link' activeClassName='active' exact>
                     Notes
                 </NavLink>
-                <NavLink to="/add" className='link' activeClassName='active' exact>
+                <NavLink to="/addnotes" className='link' activeClassName='active'>
                     Add Notes
                 </NavLink>
             </div>
